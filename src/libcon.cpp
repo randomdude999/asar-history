0db4ec1d4f12eafb2f33b4f0705a089890666dad
Alcaro <floating@muncher.se>
2012-07-09 17:23:42 +0300

    v1.26
    This isn't actually the real 1.26: it seems to contain a bit of WIP code
    related to spc7110 support, probably written by p4plus2. It also doesn't
    have the test suite or the DLL interface. But it still gives a good
    overview of the state of old Asar.

---
+++ src/libcon.cpp
@@ -0,0 +1,151 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "libcon.h"
+#include <signal.h>
+
+static char * progname;
+static char ** args;
+static int argsleft;
+static bool optional;
+bool libcon_interactive;
+static const char * usage;
+
+static volatile bool confirmclose=true;
+static void myexit()
+{
+	if (confirmclose)
+	{
+		printf("Press Enter to continue");
+		confirmclose=false;
+		getchar();
+	}
+}
+
+void libcon_badusage()
+{
+	printf("usage: %s %s", progname, usage);
+	exit(1);
+}
+
+static const char * getarg(bool tellusage, const char * defval=NULL)
+{
+	if (argsleft)
+	{
+		args++;
+		argsleft--;
+		return args[0];
+	}
+	if (tellusage) libcon_badusage();
+	return defval;
+}
+
+static const char * getfname(bool tellusage, const char * defval=NULL)
+{
+	return getarg(tellusage, defval);
+	//char * rval=malloc(char, 256);
+	//char * rvalend=rval;
+	//*rval=0;
+	//while (!strchr(rval, '.'))
+	//{
+	//	char * thisword=getarg(false, NULL);
+	//	if (!thisword)
+	//	{
+	//		if (tellusage) libcon_badusage();
+	//		else return defval;
+	//	}
+	//	if (rval!=rvalend) *(rvalend++)=' ';
+	//	rvalend+=sprintf(rvalend, "%s", thisword);
+	//}
+	//return rval;
+}
+
+static const char * requirestrfromuser(const char * question)
+{
+	confirmclose=false;
+	char * rval=(char*)malloc(256);
+	*rval=0;
+	while (!strchr(rval, '\n') || *rval=='\n')
+	{
+		printf("%s ", question);
+		fgets(rval, 250, stdin);
+	}
+	*strchr(rval, '\n')=0;
+	confirmclose=true;
+	return rval;
+}
+
+static const char * requeststrfromuser(const char * question, const char * defval)
+{
+	confirmclose=false;
+	char * rval=(char*)malloc(256);
+	*rval=0;
+	printf("%s ", question);
+	fgets(rval, 250, stdin);
+	*strchr(rval, '\n')=0;
+	confirmclose=true;
+	if (!*rval) return defval;
+	return rval;
+}
+
+void libcon_init(int argc, char ** argv, const char * usage_)
+{
+	progname=argv[0];
+	args=argv;
+	argsleft=argc-1;
+	usage=usage_;
+	libcon_interactive=(!argsleft);
+	if (libcon_interactive) atexit(myexit);
+}
+
+const char * libcon_require(const char * desc)
+{
+	if (libcon_interactive) return requirestrfromuser(desc);
+	else return getarg(true);
+}
+
+const char * libcon_require_filename(const char * desc)
+{
+	if (libcon_interactive) return requirestrfromuser(desc);
+	else return getfname(true);
+}
+
+const char * libcon_optional(const char * desc, const char * defval)
+{
+	if (libcon_interactive) return requeststrfromuser(desc, defval);
+	else return getarg(false, defval);
+}
+
+const char * libcon_optional_filename(const char * desc, const char * defval)
+{
+	if (libcon_interactive) return requeststrfromuser(desc, defval);
+	else return getfname(false, defval);
+}
+
+const char * libcon_option()
+{
+	if (!libcon_interactive && args[1][0]=='-') return getarg(false);
+	return NULL;
+}
+
+const char * libcon_question(const char * desc, const char * defval)
+{
+	if (libcon_interactive) return libcon_optional(desc, defval);
+	return defval;
+}
+
+bool libcon_question_bool(const char * desc, bool defval)
+{
+	if (!libcon_interactive) return defval;
+	while (true)
+	{
+		const char * answer=requeststrfromuser(desc, defval?"y":"n");
+		if (!stricmp(answer, "y") || !stricmp(answer, "yes")) return true;
+		if (!stricmp(answer, "n") || !stricmp(answer, "no")) return false;
+	}
+}
+
+void libcon_end()
+{
+	if (!libcon_interactive && argsleft) libcon_badusage();
+}
